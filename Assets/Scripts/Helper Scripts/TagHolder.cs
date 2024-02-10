39054769d300bede0859b76a64616e8eb01d125b
vogel <aalmonzer@gmail.com>
2024-02-10 20:43:30 +0200

    Initial commit

---
+++ Assets/Scripts/Helper Scripts/TagHolder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis {
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+}
+
+
+public class MouseAxis
+{
+    public const string X = "Mouse X";
+    public const string Y = "Mouse Y";
+}
---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+    public float speed = 5.0f;
+    private float gravity = 20.0f;
+    public float jumpForce = 10.0f;
+    private float verticalVelocity;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // Vector3 holds x, y, z. We're taking input forward and back, left and right. (x and z).
+        // We're not moving up and down, so we're setting y to 0.
+        moveDirection = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        moveDirection *= speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        characterController.Move(moveDirection);
+    }
+
+    void ApplyGravity()
+    {
+        verticalVelocity = verticalVelocity - gravity * Time.deltaTime;
+        // jump
+        PlayerJump();
+
+        moveDirection.y = verticalVelocity * Time.deltaTime;
+    } 
+
+    void PlayerJump()
+    {
+        if(characterController.isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            verticalVelocity = jumpForce;
+
+        }
+    }
+}
